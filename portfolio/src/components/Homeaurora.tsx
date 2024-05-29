aa691ca347979dffb304b6d4bfaa30ce11b265eb
Atharva Raj Thakur <69038796+atharva00721@users.noreply.github.com>
2024-05-30 05:25:10 +0530

    cleaned

--- portfolio/src/components/Homeaurora.tsx
+++ portfolio/src/components/Homeaurora.tsx
@@ -11,6 +11,7 @@ export function AuroraBackgroundDemo() {
       <BackgroundGradientAnimation>
         <div className="absolute z-30 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-1xl text-center">
           <p className="drop-shadow-2xl text-gradient-to-b from-white/80 to-white/20 text-8xl sm:text-7xl md:text-9xl">
+            
             <div>
               <TypeAnimation
                 preRenderFirstString={true}
@@ -28,6 +29,7 @@ export function AuroraBackgroundDemo() {
                 repeat={Infinity}
               />
             </div>
+            {/* <p className="uppercase text-3xl mt-5">I am just another coder(but better)</p> */}
           </p>
         </div>
       </BackgroundGradientAnimation>
